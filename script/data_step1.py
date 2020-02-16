3ad387565e6effd3d01b37a42e08f8165009ae06
Evan Chen <56132958@qq.com>
2020-02-16 14:33:47 +0800

    Update data_step1.py

--- script/data_step1.py
+++ script/data_step1.py
@@ -1,6 +1,6 @@
 # 本脚本是针对中国新冠病毒各省市历史发病数据的清洗工具
 # 作者 https://github.com/Avens666  mail: cz_666@qq.com
-# 源数据来自 https://raw.githubusercontent.com/BlankerL/DXY-COVID-19-Data/master/csv/DXYArea.csv
+# 源数据来自 https://github.com/BlankerL/DXY-COVID-19-Data/blob/master/csv/DXYArea.csv
 # 本脚本将各省市每天的数据进行去重处理，每个省市只保留最新的一条数据 （也可选择保留当天最大数值）
 # 用户通过修改 inputfile  和  outputfile 定义源数据文件和输出文件
 
